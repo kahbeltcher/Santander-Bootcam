0eecb4e0b0ab23d54c0e73459ec9573db8387511
kahbeltcher <81941451+kahbeltcher@users.noreply.github.com>
2022-07-21 06:55:36 -0300

    Delete modulos directory

--- modulos/assets/main.mjs
+++
@@ -1,5 +0,0 @@
-import {mostraIdade,mostraCidade,mostraHobby} from "./funcoes.mjs";
-
-console.log(mostraIdade('Karen',28));
-console.log(mostraCidade('Karen','Rio de Janeiro.'));
-console.log(mostraHobby('Karen','ilustração.'));
